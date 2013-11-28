c423487002d24a4483bda297918b73bffc542c07
Alex Crowe <alex@pancentric.com>
2013-11-28 12:04:13 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+"""
+sentry-gitlab
+=============
+
+An extension for Sentry which integrates with GitLab. Specifically, it allows
+you to easily create issues from events within Sentry.
+
+:copyright: (c) 2012 Pancentric Ltd, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+"""
+from setuptools import setup, find_packages
+
+
+tests_require = [
+    'nose',
+]
+
+install_requires = [
+    'sentry>=5.0.0',
+]
+
+setup(
+    name='sentry-gitlab',
+    version='0.0.1',
+    author='Alex Crowe',
+    author_email='alex@pancentric.com',
+    url='http://github.com/pancentric/sentry-gitlab',
+    description='A Sentry extension which integrates with GitLab.',
+    long_description=__doc__,
+    license='BSD',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    zip_safe=False,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
+    test_suite='runtests.runtests',
+    include_package_data=True,
+    entry_points={
+        'sentry.apps': [
+            'gitlab = sentry_gitlab',
+        ],
+        'sentry.plugins': [
+            'gitlab = sentry_gitlab.plugin:GitLabPlugin'
+        ],
+    },
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
+)
---
+++ src/sentry_gitlab/plugin.py
@@ -0,0 +1,86 @@
+"""
+sentry_gitlab.plugin
+~~~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2012 by the Sentry Team, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+"""
+
+from django import forms
+from django.utils import simplejson
+from django.utils.translation import ugettext_lazy as _
+from sentry.plugins.bases.issue import IssuePlugin
+from gitlab import *
+
+import sentry_gitlab
+
+
+
+class GitLabOptionsForm(forms.Form):
+    gitlab_url = forms.CharField(label=_('GitLab URL'),
+        widget=forms.TextInput(attrs={'placeholder': 'e.g. https://gitlab.example.com'}),
+        help_text=_('Enter your GitLab URL here'))
+
+    gitlab_token = forms.CharField(label=_('GitLab Private Token'),
+        widget=forms.TextInput(attrs={'placeholder': 'e.g. g5DWFtLzaztgYFrqhVfE'}),
+        help_text=_('Enter your GitLab Private Token'))
+
+    gitlab_repo = forms.CharField(label=_('Repository Name'),
+        widget=forms.TextInput(attrs={'placeholder': 'e.g. pancentric/repo'}),
+        help_text=_('Enter your repository name, including the owner.'))
+
+
+class GitLabPlugin(IssuePlugin):
+    author = 'Pancentric Ltd'
+    author_url = 'https://github.com/pancentric/sentry-gitlab'
+    version = sentry_gitlab.VERSION
+    description = "Integrate GitLab issues by linking a repository to a project"
+    resource_links = [
+        ('Bug Tracker', 'https://github.com/pancentric/sentry-gitlab/issues'),
+        ('Source', 'https://github.com/pancentric/sentry-gitlab'),
+    ]
+
+    slug = 'gitlab'
+    title = _('GitLab')
+    conf_title = title
+    conf_key = 'gitlab'
+    project_conf_form = GitLabOptionsForm
+
+    def is_configured(self, request, project, **kwargs):
+        return bool(self.get_option('repo', project))
+
+    def get_new_issue_title(self, **kwargs):
+        return 'Create GitLab Issue'
+
+    def create_issue(self, request, group, form_data, **kwargs):
+
+        url = group.project.gitlab_url
+        token = group.project.gitlab_token
+        repo = group.project.gitlab_repo
+
+        gl = Gitlab(url, token)
+        try:
+            gl.auth()
+        except GitlabAuthenticationError:
+            raise forms.ValidationError(_('Unauthorized: Invalid Private Token: %s') % (e,))
+        except Exception:
+            raise forms.ValidationError(_('Error Communicating with GitLab: %s') % (e,))
+
+        data = simplejson.dumps({
+            "title": form_data['title'],
+            "description": form_data['description']
+        })
+
+        proj = gl.Project(id=repo)
+        issue = proj.Issue(data)
+        issue.save()
+
+
+    def get_issue_label(self, group, issue_id, **kwargs):
+        return 'GL-%s' % issue_id
+
+    def get_issue_url(self, group, issue_id, **kwargs):
+        gitlab_url = self.get_option('gitlab_url', group.project)
+        repo = self.get_option('repo', group.project)
+
+        return '%s/%s/issues/%s' % (gitlab_url, repo, issue_id)

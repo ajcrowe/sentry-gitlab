86c7a5d71046a5d8dc32b91eb9f1241d084c950c
Alex Crowe <alex@pancentric.com>
2013-11-29 10:15:00 +0000

    added more unicode to str conversion

--- src/sentry_gitlab/plugin.py
+++ src/sentry_gitlab/plugin.py
@@ -76,7 +76,7 @@ class GitLabPlugin(IssuePlugin):
         except Exception:
             raise forms.ValidationError(_('Error Communicating with GitLab: %s') % (e,))
 
-        data = ("{'title': '%s', 'description': '%s'}" % (form_data['title'], form_data['description']))
+        data = str(("{'title': '%s', 'description': '%s'}" % (form_data['title'], form_data['description'])))
 
         proj = gl.Project(id=repo_url)
         issue = proj.Issue(data)
